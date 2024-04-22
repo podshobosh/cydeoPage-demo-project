716cd3c2f446f46ab74d66d2061ea801debcd72b
podshobosh <podsho1704@gmail.com>
2024-04-22 17:19:35 -0400

    added runner classes

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,26 @@
+package com.cydeo.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin =  {"json:target/cucumber.json",
+                   "html:target/cucumber-reports.html",
+                   "rerun:target/rerun.txt",
+                   "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = {"com/demo/step_definition"},
+        dryRun = false,
+        tags = "@index",
+        publish = true // generates the report with public link
+
+
+
+
+)
+public class CukesRunner{
+}
