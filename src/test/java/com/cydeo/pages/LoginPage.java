7d6dde4caed930d857e31f9158abc4b9e5220445
podshobosh <podsho1704@gmail.com>
2024-04-23 10:33:00 -0400

    POM for LoginPage

---
+++ src/test/java/com/cydeo/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(name = "username")
+    public WebElement username;
+
+    @FindBy(name = "password")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[.='Login']")
+    public WebElement loginButton;
+
+
+    public void login(){
+        username.sendKeys(ConfigurationReader.getProperty("username"));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
+    }
+
+    public void login(String username, String password){
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        this.loginButton.click();
+    }
+}
